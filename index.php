30fb821fb497a6268faae47669de6d45b0cca107
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-27 11:31:05 +0200

    jour 1 .2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 //affiche une chaine de caracteres qui contient du html
 $toto = '<h1>12</h1>';
 echo $toto;
@@ -38,7 +39,7 @@ Quelque regle :
 - le nom d'une variable contient, des chiffres ,des underscores et c'est tout
 - le nom d'une variable ne contient pas d'espace
 
-*/
+*/ /*
 
 $jour ="mardi";
 $jourTest = $jour = "mercredi";
@@ -60,4 +61,37 @@ echo $a;
 - Ressource
 - Enumeration ou Enums
 
+*/ 
+
+/* Les Operateurs
+- additions +
+- soustraction -
+- multiplication *
+- division /
+- modulo %
+-  ** (10 puissance 2 : 10 ** 2)
 */
+
+$x = $x /4 ;
+$x /= 4;
+
+ /* Operateur combinés 
+$i = $i + 1;
+$i++;
+$i +=1;
+*/
+
+/* Operateur de comparaison
+- == compare la valeur
+- === compare la valeur et le type
+- != ou <> différent en valeur
+- !== different en valeur et en type
+- <
+- >
+- <=
+- =>
+*/
+
+// Exemple de vérification d'opérateur de comparaison avec la fonction var_dump()
+$x=42;
+var_dump($x === '42');
